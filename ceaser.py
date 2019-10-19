a538158d016b436b658050ac60f2705232ccbd1c
nick <nwrner516@gmail.com>
2019-10-18 20:33:17 -0600

    Added ceaser start 2.

--- ceaser.py
+++ ceaser.py
@@ -1,2 +1,20 @@
-print("Hello world!")
-print("This is a change.")
+english_alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", 'i', "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+                    "t", "u", "v", "w", "x", "y", "z"]
+message = "This is a message to be encrypted."
+key = 3
+
+
+message = message.replace(" ","").lower()
+encrypted_message = ""
+for letters in message:
+    try:
+        offset = english_alphabet.index(letters)+key
+        if offset > len(english_alphabet):
+            new_offset = offset-len(english_alphabet)
+            encrypted_message += english_alphabet[new_offset]
+        else:
+            encrypted_message += english_alphabet[offset]
+    except ValueError:
+        pass
+
+print(encrypted_message)
